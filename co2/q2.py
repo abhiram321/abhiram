92b867749ec3a208f5d0828173994c5f319ba5b5
abhiram321 <93858141+abhiram321@users.noreply.github.com>
2021-12-01 22:36:45 +0530

    Add files via upload

---
+++ co2/q2.py
@@ -0,0 +1,14 @@
+n=int(input("enter the number"))
+a=0
+b=1
+count=1
+sum=0
+while(n<=count):
+    print(sum,end="")
+    count=1+count
+    a=b
+    b=sum
+    sum=a+b
+    
+
+
---
+++ co2/q3.py
@@ -0,0 +1,3 @@
+list=[10,40,50,30]
+total=sum(list)
+print("total is",total)
