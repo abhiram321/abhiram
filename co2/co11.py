b2cbc59010c5b80489c8e87d735394f8eaabd537
abhiram321 <93858141+abhiram321@users.noreply.github.com>
2021-12-08 16:01:53 +0530

    Add files via upload

---
+++ co2/co11.py
@@ -0,0 +1,6 @@
+t_area=lambda t,h:1/2*t*h
+r_area=lambda l,b:l*b
+s_area=lambda a:a*a
+print("area of triangke",t_area(10,20))
+print("area of rectangle",r_area(10,20))
+print("area of square",s_area(10))
