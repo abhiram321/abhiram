d736cd347c6e9fb19df71d5b12c04043eebaac34
abhiram321 <93858141+abhiram321@users.noreply.github.com>
2022-01-18 22:54:58 +0530

    Add files via upload

---
+++ co3/co3_1.py
@@ -0,0 +1,11 @@
+import math
+print("The value of pi ",math.pi)
+
+import math as m
+print("The value of pi ",m.pi)
+
+from math import pi,sqrt
+print(math.pi)
+print(math.sqrt(5))
+print(math.tan(30))
+print(math.cos(45))
